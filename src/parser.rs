aae17030d87b4c8b4e81b197c760ed51b4162875
Stephen Becker IV <github@deathbyescalator.com>
2015-07-26 22:32:45 -0700

    I wouldnt be damned it almost works
    nom mostly working. extra new lines seems to be the issue right now.

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,23 @@
-use nom::{IResult,not_line_ending, space, alphanumeric, multispace, digit, Needed, Err};
+use nom::{IResult,not_line_ending, line_ending, space, alphanumeric, multispace, digit, Needed, Err};
 use nom::IResult::*;
 // Parser definition
 use std::str;
-use std::str::FromStr;
+use std::collections::HashMap;
+
 fn version_number(input: &[u8]) -> IResult<&[u8], &[u8]> {
     for (idx, chr) in input.iter().enumerate() {
         match *chr {
             46 | 48...57  => continue,
+            _ => return IResult::Done(&input[idx..], &input[..idx]),
+        }
+    }
+    IResult::Incomplete(Needed::Size(1))
+}
+
+fn token_colon(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    for (idx, chr) in input.iter().enumerate() {
+        match *chr {
+            33...126 => continue,
             _ => return IResult::Done(&input[idx..], &input[..idx]),
         }
     }
@@ -21,34 +32,66 @@ fn token(input: &[u8]) -> IResult<&[u8], &[u8]> {
     }
     IResult::Incomplete(Needed::Size(1))
 }
-named!(pub init_line <&[u8], (&[u8])>,
+named!(pub init_line <&[u8], (&str, &str)>,
 dbg!(chain!(
+        line_ending?                ~
         tag!("WARC")                ~
         tag!("/")                   ~
         space?                      ~
-        version: version_number     ~
+        version: map_res!(version_number, str::from_utf8)~
         tag!("\r")?                 ~
         tag!("\n")                  ,
-        || {(version)}
+        || {("WARCVERSION", version)}
         )
     ));
-named!(pub header_match <&[u8], (&[u8], &[u8])>,
+named!(pub header_match <&[u8], (&str, &str)>,
 dbg!(chain!(
-        name: token                 ~
+        name: map_res!(token, str::from_utf8)~
         space?                      ~
         tag!(":")                   ~
         space?                      ~
-        value: token                ~
+        value: map_res!(token_colon, str::from_utf8)~
         tag!("\r")?                 ~
         tag!("\n")                  ,
         || {(name, value)}
         )
     ));
-named!(pub header_aggregator<&[u8], Vec<(&[u8],&[u8])> >, many1!(header_match));
-named!(pub warc_header<&[u8], ((&[u8]), Vec<(&[u8],&[u8])>) >,
+
+named!(pub header_aggregator<&[u8], Vec<(&str,&str)> >, many1!(header_match));
+
+named!(pub warc_header<&[u8], ((&str, &str), Vec<(&str,&str)>) >,
 chain!(
     version: init_line              ~
     headers: header_aggregator      ,
     move ||{(version, headers)}
     )
 );
+
+
+named!(pub warc_records<&[u8], Vec<HashMap<&str,&str>> >, many1!(warc_record));
+pub fn warc_record(input: &[u8]) -> IResult<&[u8], HashMap<&str,&str>>{
+    let mut h: HashMap<&str,  &str> = HashMap::new();
+    match warc_header(input) {
+        IResult::Done(mut i,tuple_vec) => {
+            let (name, version) = tuple_vec.0;
+            h.insert(name, version);
+            let headers =  tuple_vec.1;
+            for &(k,ref v) in headers.iter() {
+                h.insert(k, v.clone());
+            }
+            match h.get(&"Content-Length"){
+                Some(&length) => {
+                    let length_number = length.parse::<u32>().unwrap();
+                    let content = &i[0..length_number as usize];
+                    let content_str = str::from_utf8(content).unwrap();
+                    i = &i[length_number as usize ..];
+                    h.insert(&"CONTENT", content_str);
+                }
+                _ => println!("shit no content-length")
+            }
+            IResult::Done(i, h)
+        },
+        IResult::Incomplete(a)     => IResult::Incomplete(a),
+        IResult::Error(a)          => IResult::Error(a)
+    }
+}
